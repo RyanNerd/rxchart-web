ce05052d2851d7a64adbdebdd63c2c0499462807
ryan <ryan.jentzsch@gmail.com>
2020-10-12 15:54:23 -0600

    :wrench: Fix code indentation in MedicineDetail

--- src/components/Grids/MedicineDetail.tsx
+++ src/components/Grids/MedicineDetail.tsx
@@ -28,42 +28,42 @@ const MedicineDetail = (props: IProps): JSX.Element => {
             id={'med-detail-grid-row-' + drug.Id}
         >
             {onEdit &&
-            <td style={{textAlign: 'center', verticalAlign: "middle"}}>
-                < Button
-                    size="sm"
-                    id={"medicine-edit-btn-" + drug.Id}
-                    onClick={(e) => onEdit(e, drug)}
-                >
-                    Edit
-                </Button>
-            </td>
+                <td style={{textAlign: 'center', verticalAlign: "middle"}}>
+                    < Button
+                        size="sm"
+                        id={"medicine-edit-btn-" + drug.Id}
+                        onClick={(e) => onEdit(e, drug)}
+                    >
+                        Edit
+                    </Button>
+                </td>
             }
             {columns.includes('Drug') &&
                 <td>{drug.Drug}</td>
             }
             {columns.includes('Strength') &&
-            <td>{drug.Strength}</td>
+                <td>{drug.Strength}</td>
             }
             {columns.includes('Directions') &&
-            <td>{drug.Directions}</td>
+                <td>{drug.Directions}</td>
             }
             {columns.includes('Notes') &&
                 <td>{drug.Notes}</td>
             }
             {columns.includes('Barcode') &&
-            <td>{drug.Barcode}</td>
+                <td>{drug.Barcode}</td>
             }
             {onDelete &&
-            <td style={{textAlign: 'center', verticalAlign: "middle"}}>
-                <Button
-                    size="sm"
-                    id={"medicine-grid-delete-btn-" + drug.Id}
-                    variant="outline-danger"
-                    onClick={(e) => onDelete(e, drug)}
-                >
-                    <span role="img" aria-label="delete">🗑️</span>
-                </Button>
-            </td>
+                <td style={{textAlign: 'center', verticalAlign: "middle"}}>
+                    <Button
+                        size="sm"
+                        id={"medicine-grid-delete-btn-" + drug.Id}
+                        variant="outline-danger"
+                        onClick={(e) => onDelete(e, drug)}
+                    >
+                        <span role="img" aria-label="delete">🗑️</span>
+                    </Button>
+                </td>
             }
         </tr>
     )
