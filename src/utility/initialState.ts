9b22d958ff6623e552c3293f70a025b697cd2c15
ryan <ryan.jentzsch@gmail.com>
2020-10-15 09:40:31 -0600

    :lipstick: cosmetic code changes in

--- src/utility/initialState.ts
+++ src/utility/initialState.ts
@@ -1,17 +1,21 @@
 import {ProviderTypes} from '../types/ProviderTypes';
+import {DrugLogRecord, MedicineRecord, ResidentRecord} from "../types/RecordTypes";
+import ResidentProvider from "../providers/ResidentProvider";
+import MedicineProvider from "../providers/MedicineProvider";
+import MedHistoryProvider from "../providers/MedHistoryProvider";
 
 export const initialState = {
     development: process.env.REACT_APP_DEVELOPMENT === 'true',
-    activeResident: null,
-    residentList: [],
-    medicineList: [],
-    otcList: [],
-    drugLogList: [],
-    apiKey: null,
+    activeResident: null as ResidentRecord | null,
+    residentList: [] as ResidentRecord[],
+    medicineList: [] as MedicineRecord[],
+    otcList: [] as MedicineRecord[],
+    drugLogList: [] as DrugLogRecord[],
+    apiKey: null as string | null,
     baseUrl: process.env.REACT_APP_BASEURL,
     providers: {
-        residentProvider: null,
-        medicineProvider: null,
-        medHistoryProvider: null,
+        residentProvider: null as typeof ResidentProvider | null,
+        medicineProvider: null as typeof MedicineProvider | null,
+        medHistoryProvider: null as typeof MedHistoryProvider | null,
     } as ProviderTypes.Providers,
 };
