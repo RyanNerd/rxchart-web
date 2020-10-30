807933c558f1ba7b4dff7c43e8c89865940d2dc5
ryan <ryan.jentzsch@gmail.com>
2020-10-30 10:40:02 -0600

    Add 📜 Scroll-to-top logic in the ResidentPage when it becomes active

--- src/components/Pages/ResidentPage.tsx
+++ src/components/Pages/ResidentPage.tsx
@@ -71,6 +71,8 @@ const ResidentPage = (props: IProps): JSX.Element | null => {
     // Don't render if this tab isn't active.
     if (activeTabKey !== 'resident') {
         return null;
+    } else {
+        window.scrollTo({ top: 0, behavior: 'smooth' });
     }
 
     /**
