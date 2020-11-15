2385d688e69dd20105670f7c898cb2919164e904
ryan <ryan.jentzsch@gmail.com>
2020-11-15 03:43:56 -0700

    ♻ When a new client is added make that client active

--- src/observers/UpdateClientObserver.ts
+++ src/observers/UpdateClientObserver.ts
@@ -10,20 +10,26 @@ import {useEffect, useGlobal} from "reactn";
 const UpdateClientOvserver = (rm: IResidentManager) => {
     const [, setErrorDetails] = useGlobal('errorDetails');
     const [, setRefreshClients] = useGlobal('refreshClients');
+    const [, setActiveResident] = useGlobal('activeResident');
     const [updateClient, setUpdateClient] = useGlobal('updateClient');
 
     useEffect(() => {
         if (updateClient) {
+            console.log('updateClient', updateClient);
             rm.updateResident(updateClient)
-            .then(() => {
+            .then((residentRecord) => {
                 setRefreshClients(true);
+                // When adding a new client set that client as active
+                if (!updateClient.Id) {
+                    setActiveResident(residentRecord);
+                }
             })
             .then(() => {
                 setUpdateClient(null)
             })
             .catch((err) => setErrorDetails(err))
         }
-    }, [updateClient, setUpdateClient, rm, setRefreshClients, setErrorDetails]);
+    }, [setActiveResident, updateClient, setUpdateClient, rm, setRefreshClients, setErrorDetails]);
 }
 
 export default UpdateClientOvserver;
