17ec70b2586a178a9ace72c086676203026b25f6
ryan <ryan.jentzsch@gmail.com>
2021-05-15 04:13:08 -0600

    Add useStickyState hook for possible future use

---
+++ src/hooks/useStickyState.tsx
@@ -0,0 +1,16 @@
+import React, {useState, useEffect} from 'react';
+
+const useStickyState = (defaultValue: any, key: string) => {
+    const [value, setValue] = useState(() => {
+        const stickyValue = window.localStorage.getItem(key);
+        return stickyValue !== null
+            ? JSON.parse(stickyValue)
+            : defaultValue;
+    });
+    useEffect(() => {
+        window.localStorage.setItem(key, JSON.stringify(value));
+    }, [key, value]);
+    return [value, setValue];
+}
+
+export default useStickyState;
