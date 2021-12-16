c23807d84a0ee013f25d1448f8f5ec343a5cec8a
ryan <ryan.jentzsch@gmail.com>
2021-12-16 14:32:25 -0700

    🌀 refactor Change app start up to use async/await instead of Promises

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,9 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import App from 'components/App/App';
-import getInitialState from './utility/getInitialState';
-import React, {setGlobal} from 'reactn';
 import ReactDOM from 'react-dom';
+import React, {setGlobal} from 'reactn';
+import {State} from 'reactn/default';
+import getInitialState from './utility/getInitialState';
 
 /**
  * This gets called if something went really, really wrong at the launch of the app
@@ -16,18 +17,29 @@ const InitError = () => {
     );
 };
 
-setGlobal(getInitialState())
-    .then((initialState) => {
+/**
+ * Start the app using async/await
+ */
+const startApp = async () => {
+    /**
+     * Fires once the App has successfully rendered logging the result to the console
+     * @param {State} initialState The global initial state
+     */
+    const logAppStarted = (initialState: State) => {
         if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
             console.log('RxChart Started', initialState);
         } else {
             console.log('RxChart Started');
         }
-    })
-    .then(() => {
-        ReactDOM.render(<App />, document.getElementById('root'));
-    })
-    .catch((err) => {
+    };
+
+    try {
+        const initialState = await setGlobal(getInitialState());
+        ReactDOM.render(<App />, document.getElementById('root'), () => logAppStarted(initialState));
+    } catch (err) {
         console.log('Something went wrong', err);
         ReactDOM.render(<InitError />, document.getElementById('root'));
-    });
+    }
+};
+
+startApp();
