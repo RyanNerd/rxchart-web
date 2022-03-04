be41efdc18d5dcc1f9db76b6e983478192fc9f4e
ryan <ryan.jentzsch@gmail.com>
2022-03-04 03:22:54 -0700

    ⛲ feat Fleshed out the FileEdit modal UI with validations and warnings
    - Added read-only fields: Size, Modified Date, and Media Type
    - A warning alert will show if the user changes the file extension

--- src/components/Pages/Modals/FileEdit.tsx
+++ src/components/Pages/Modals/FileEdit.tsx
@@ -1,10 +1,13 @@
+import Alert from 'react-bootstrap/Alert';
 import Button from 'react-bootstrap/Button';
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
 import Col from 'react-bootstrap/Col';
 import Form from 'react-bootstrap/Form';
 import Modal from 'react-bootstrap/Modal';
 import Row from 'react-bootstrap/Row';
 import React, {useEffect, useRef, useState} from 'reactn';
 import {FileRecord} from 'types/RecordTypes';
+import {getFormattedDate} from 'utility/common';
 
 interface IProps {
     fileInfo: FileRecord;
@@ -16,6 +19,11 @@ interface IProps {
 const FileEdit = (props: IProps) => {
     const textInput = useRef<HTMLInputElement>(null);
     const {onHide, onClose} = props;
+    const [showFileExtensionWarning, setShowFileExtensionWarning] = useState(false);
+
+    const getFileExtension = (fileName: string) => {
+        return fileName ? fileName.split('.').pop() : '';
+    };
 
     const [fileInfo, setFileInfo] = useState(props.fileInfo);
     useEffect(() => {
@@ -31,9 +39,12 @@ const FileEdit = (props: IProps) => {
         if (!fileInfo || fileInfo.FileName?.length === 0) {
             setCanSave(false);
         } else {
+            setShowFileExtensionWarning(
+                getFileExtension(fileInfo.FileName) !== getFileExtension(props?.fileInfo?.FileName || '')
+            );
             setCanSave(true);
         }
-    }, [fileInfo]);
+    }, [fileInfo, props?.fileInfo?.FileName]);
 
     const [show, setShow] = useState(props.show);
     useEffect(() => {
@@ -78,7 +89,7 @@ const FileEdit = (props: IProps) => {
             </Modal.Header>
 
             <Modal.Body>
-                <Form>
+                <Form noValidate>
                     <Form.Group as={Row} controlId="formFileName">
                         <Form.Label column sm="2">
                             File Name
@@ -86,16 +97,18 @@ const FileEdit = (props: IProps) => {
                         <Col sm="10">
                             <Form.Control
                                 autoComplete="off"
+                                className={fileInfo.FileName?.length > 0 ? '' : 'is-invalid'}
                                 type="input"
                                 name="FileName"
                                 ref={textInput}
                                 onChange={(changeEvent) => handleOnChange(changeEvent)}
                                 value={fileInfo.FileName}
                             />
+                            <Form.Control.Feedback type="invalid">File Name can not be blank</Form.Control.Feedback>
                         </Col>
                     </Form.Group>
 
-                    <Form.Group as={Row}>
+                    <Form.Group as={Row} controlId="formDescription">
                         <Form.Label column sm="2">
                             Description
                         </Form.Label>
@@ -109,16 +122,45 @@ const FileEdit = (props: IProps) => {
                             />
                         </Col>
                     </Form.Group>
+
+                    <Form.Group as={Row} controlId="formSize">
+                        <Form.Label column sm="1">
+                            Size
+                        </Form.Label>
+                        <Col sm="3">
+                            <Form.Control readOnly={true} value={fileInfo.Size?.toString()} />
+                        </Col>
+                        <Form.Label column sm="1">
+                            Modified
+                        </Form.Label>
+                        <Col sm="3">
+                            <Form.Control
+                                readOnly={true}
+                                value={fileInfo ? getFormattedDate(fileInfo.Updated || '') : ''}
+                            />
+                        </Col>
+                        <Form.Label column sm="1">
+                            Type
+                        </Form.Label>
+                        <Col sm="3">
+                            <Form.Control readOnly={true} value={fileInfo?.MediaType || ''} />
+                        </Col>
+                    </Form.Group>
                 </Form>
             </Modal.Body>
 
             <Modal.Footer>
-                <Button onClick={() => handleHide(false)} variant="secondary">
-                    Cancel
-                </Button>
-                <Button disabled={!canSave} onClick={() => handleHide(true)} variant="primary">
-                    Save changes
-                </Button>
+                <Alert variant="warning" show={showFileExtensionWarning}>
+                    WARNING: The file extension changed. This may cause problems.
+                </Alert>
+                <ButtonGroup>
+                    <Button onClick={() => handleHide(false)} variant="secondary">
+                        Cancel
+                    </Button>
+                    <Button disabled={!canSave} onClick={() => handleHide(true)} variant="primary">
+                        Save changes
+                    </Button>
+                </ButtonGroup>
             </Modal.Footer>
         </Modal>
     );
